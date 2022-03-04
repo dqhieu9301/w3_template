d784d491be98abcfecdf1e57c7e5a8a73fcbf6b8
your name <dinhhieu9301@gmail.com>
2022-03-04 22:38:18 +0700

    update mobile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss'
 import '../../themify-icons-font/themify-icons/themify-icons.css'
 const Header = () => {
+    // open-close menu mobile
+    const [openMenu, setOpenMenu] = useState(false)
+    const handleOnclickMenu = () => {
+        setOpenMenu(!openMenu)
+    }
     return (
-        <div className='header'>
+        <div className='header' style={{ height: openMenu ? 'auto' : '38px' }}>
             <div className="nav">
                 <ul className='navbar'>
                     <li>
@@ -24,6 +29,7 @@ const Header = () => {
                 </ul>
             </div>
             <div className='ti-search'></div>
+            <div className='ti-menu' onClick={handleOnclickMenu}></div>
         </div>
     )
 }
