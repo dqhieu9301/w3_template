8ee3bdf6e5a8381e3803221c04020ce27221f362
your name <dinhhieu9301@gmail.com>
2022-03-05 18:37:19 +0700

    fix bug all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/header';
-import Content from './components/content';
+import Slider from './components/content/slider';
+import ContentOne from './components/content/content_one';
+import ContentTwo from './components/content/content_two';
+import ContenThree from './components/content/content_three';
+import Information from './components/content/content_information';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Slider />
+      <ContentOne />
+      <ContentTwo />
+      <ContenThree />
+      <Information />
     </div>
   );
 }
